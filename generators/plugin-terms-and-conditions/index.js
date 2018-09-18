eb2a31550478e4b204d3628b1874af7b05d8925f
bogdan_scher <bsc@piogroup.com>
2018-09-18 17:28:52 +0300

    add Terms sub-generator

---
+++ generators/plugin-terms-and-conditions/index.js
@@ -0,0 +1,56 @@
+'use strict';
+const Generator = require('yeoman-generator');
+const chalk = require('chalk');
+const yosay = require('yosay');
+const process = require('process');
+
+const functions = require('./functions');
+const commonFunctions = require('../../common/functions');
+
+module.exports = class extends Generator {
+  prompting() {
+    this.log(
+      yosay(`Welcome to the Terms and Conditions sub-generator!`)
+    );
+
+    const prompts = [
+      {
+        type: 'confirm',
+        name: 'someAnswer',
+        message: 'Would you like to enable this option?',
+        default: true
+      }
+    ];
+
+    return this.prompt(prompts).then(props => {
+      this.props = props;
+    });
+  }
+
+  writing() {
+      try {
+
+          commonFunctions.goToPluginsDirectory();
+
+          commonFunctions.cloneProject(
+              this,
+              'Terms and Conditions plugin',
+              'master',
+              'https://github.com/PulseTile-Plugins/Plugin-T-CsModal-HelmPHR',
+              'TermsAndConditions'
+          );
+
+          setTimeout(function () {
+              functions.replacePluginFiles();
+          }, 10000);
+
+          setTimeout(function () {
+              functions.removeThemeDirectory();
+          }, 10000);
+
+      } catch (err) {
+          console.log(yosay(`${chalk.green('ERROR: ')} ${err}`));
+          console.error(`ERROR: ${err}`);
+      }
+  }
+};
